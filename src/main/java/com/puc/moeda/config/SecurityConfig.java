5f3415826b5688ad0be3d64d0429a60198ec696e
Arthur Miranda <arthur.miranda@dtidigital.com.br>
2025-05-27 19:35:24 -0300

    el

--- src/main/java/com/puc/moeda/config/SecurityConfig.java
+++ src/main/java/com/puc/moeda/config/SecurityConfig.java
@@ -44,6 +44,7 @@ public class SecurityConfig {
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/api/auth/**").permitAll()
                 .requestMatchers("/api/register/**").permitAll()
+                .requestMatchers("/test").permitAll()
                 .anyRequest().authenticated()
             )
             .sessionManagement(session -> session
